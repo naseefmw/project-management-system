9e53be0b5f69eeb706839ff0ce28fc422edcd0ea
Naseef MP <60293453+naseefmw@users.noreply.github.com>
2022-07-29 01:29:06 +0530

    project

---
+++ client/src/Components/Pages/LoginPage/Login.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { login } from "../../../Services/userService";
+import Background from "../../Background";
+import {
+  BgContainer,
+  Container,
+  FormSection,
+  FormCard,
+  Form,
+  Title,
+  Input,
+  Button,
+  Hr,
+  Link,
+} from "./Styled";
+
+const Login = () => {
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const [userInformations, setUserInformations] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    document.title = "Log in to PMS";
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(userInformations, dispatch);
+  };
+  return (
+    <>
+      <BgContainer>
+        <Background />
+      </BgContainer>
+      <Container>
+        <FormSection>
+          <FormCard>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+              <Title>Log in to Project Management System</Title>
+              <Input
+                type="email"
+                placeholder="Enter email"
+                required
+                value={userInformations.email}
+                onChange={(e) =>
+                  setUserInformations({
+                    ...userInformations,
+                    email: e.target.value,
+                  })
+                }
+              />
+              <Input
+                type="password"
+                placeholder="Enter password"
+                required
+                value={userInformations.password}
+                onChange={(e) =>
+                  setUserInformations({
+                    ...userInformations,
+                    password: e.target.value,
+                  })
+                }
+              />
+              <Button>Log in</Button>
+              <Hr />
+              <Link
+                fontSize="0.85rem"
+                onClick={() => history.push("/register")}
+              >
+                Sign up for an account
+              </Link>
+            </Form>
+          </FormCard>
+        </FormSection>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
